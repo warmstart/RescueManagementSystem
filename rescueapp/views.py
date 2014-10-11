9a6c9c681d4635074d09c245736c48ac81a5e984
Sebastian Gebhard <sebastian.gebhard@warmstart.eu>
2014-10-11 13:35:05 +0200

    saving SMS

--- rescueapp/views.py
+++ rescueapp/views.py
@@ -1,8 +1,12 @@
 import json
+import send_sms
 
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+
+from rescueapp.models import texts
 
 # Create your views here.
 @csrf_exempt
@@ -19,9 +23,15 @@ def incomingMessage(request):
 
     ## TODO: Only return the OK status, if processing was actually ok ;) Not only when no exception is risen
 
+    text = texts(receiveTime=timezone.now(), sender=data['senderAddress'], text=data['textMessageContent'])
+    text.save()
+
     response_data = {}
     response_data['statusCode'] = 2000;
     response_data['statusMessage'] = "ok"
 
 
     return HttpResponse(json.dumps(response_data), content_type="application/json")
+
+def sendSMS(request):
+    
